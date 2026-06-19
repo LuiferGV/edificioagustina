0b84692498add46bd3484c8f51d5a8da5b2b6ed4
LuiferGV <luifer.gv@gmail.com>
2026-06-19 12:24:32 -0300

    chore: add firebase fallback config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDnuaIzQfWT7i5IKXrvAOJs695zXVFM7yI",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "edificio-agustina.firebaseapp.com",
+  databaseURL:
+    import.meta.env.VITE_FIREBASE_DATABASE_URL ||
+    "https://edificio-agustina-default-rtdb.firebaseio.com/",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "edificio-agustina",
+  storageBucket:
+    import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "edificio-agustina.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "746749808585",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:746749808585:web:c6b55442dfc5fb525c6218",
 };
 
 const app = initializeApp(firebaseConfig);
